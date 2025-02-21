fca96e8640d554ba068fca91b35dd751a9cca209
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2025-02-21 15:02:23 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://synk.digital', // Change this to your domain
+    generateRobotsTxt: true, // Generates a robots.txt file
+    exclude: ['/admin'], // Add paths you want to exclude
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
